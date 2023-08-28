75325e11fb2b43244737b3c54f244e53411c1748
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-08-28 21:42:29 +0300

    clickHandler is completed using applyNumber

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import { applyNumber } from "./actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clickHandler = (event) => {
+    dispatch(applyNumber(parseInt(event.target.value)));
+  };
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -33,21 +38,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={() => {}} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={clickHandler} />
+              <CalcButton value={2} onClick={clickHandler} />
+              <CalcButton value={3} onClick={clickHandler} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={clickHandler} />
+              <CalcButton value={5} onClick={clickHandler} />
+              <CalcButton value={6} onClick={clickHandler} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={clickHandler} />
+              <CalcButton value={8} onClick={clickHandler} />
+              <CalcButton value={9} onClick={clickHandler} />
             </div>
 
             <div className="row">
